925afa316f00076d89ad3b9745236dbbbfa895a2
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-07-09 00:01:33 -0300

    Update exercicio2.py

--- exercicio2.py
+++ exercicio2.py
@@ -2,4 +2,4 @@ num_1 = int(input('Digite um número inteiro: '))
 if num_1 % 2 == 0:
     print(f'O número {num_1} é par.')
 else:
-    print(f'O número {num_1} não é ímpar.')
+    print(f'O número {num_1} é ímpar.')
